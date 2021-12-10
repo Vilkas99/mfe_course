7c43e9e615405e957579b1e071886d2a42752102
Vilkas99 <victorium99@hotmail.com>
2021-12-10 12:06:20 -0600

    fix css

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { StylesProvider } from "@material-ui/styles";
+import { StylesProvider, createGenerateClassName } from "@material-ui/styles";
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClass = createGenerateClassName({
+	productionPrefix: 'ma',
+})
+
 export default () => {
 	return <div>
-		<StylesProvider>
+		<StylesProvider generateClassName={generateClass}>
 			<BrowserRouter>
 				<Switch>
 					<Route exact path="/pricing" component={Pricing} />
